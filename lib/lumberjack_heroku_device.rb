0f083b99f5c58665d59012e52dfcae82b05e1470
Tony Coconate <me@tonycoconate.com>
2011-12-28 23:44:14 -0600

    building gem round 3

--- lib/lumberjack_heroku_device.rb
+++ lib/lumberjack_heroku_device.rb
@@ -18,12 +18,17 @@ module Rails
   module Rack
     class LogTailer
       def initialize(app, log = nil)
+        @app = app
       end
 
       def call(env)
+        response = @app.call(env)
+        tail!
+        response
       end
 
       def tail!
+        $stdout.print "Testing... 1. 2. 3."
       end
     end
   end
